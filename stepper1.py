bb5b50e040c6d98bb9323852c2cc3c6363efaab4
Student <alvaclavering@dpengineering.org>
2021-09-24 14:29:19 -0700

    py file updated

---
+++ stepper1.py
@@ -0,0 +1,74 @@
+import spidev
+import os
+from time import sleep
+import RPi.GPIO as GPIO
+from pidev.stepper import stepper
+from Slush.Devices import L6470Registers
+spi = spidev.SpiDev()
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.animation import Animation
+
+from pidev.MixPanel import MixPanel
+from pidev.kivy.PassCodeScreen import PassCodeScreen
+from pidev.kivy.PauseScreen import PauseScreen
+from pidev.kivy import DPEAButton
+from pidev.kivy import ImageButton
+from pidev.kivy.selfupdatinglabel import SelfUpdatingLabel
+from kivy.uix.slider import Slider
+from kivy.properties import ObjectProperty
+from threading import Thread
+
+MIXPANEL_TOKEN = "x"
+MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
+
+SCREEN_MANAGER = ScreenManager()
+MAIN_SCREEN_NAME = 'stepper1'
+
+# Init a 200 steps per revolution stepper on Port 0
+s0 = stepper(port=0, micro_steps=32, hold_current=20, run_current=20, accel_current=20, deaccel_current=20,
+             steps_per_unit=200, speed=3)
+
+class ProjectNameGUI(App):
+    """
+    Class to handle running the GUI Application
+    """
+
+    def build(self):
+        """
+        Build the application
+        :return: Kivy Screen Manager instance
+        """
+        return SCREEN_MANAGER
+
+Window.clearcolor = (0.92, 0.97, 0.97, 1)  # White
+
+class MainScreen(Screen):
+    """
+    Class to handle the main screen and its associated touch events
+    """
+
+    def start_stepper(self):
+        """
+        Function that turns on the stepper
+        """
+        s0.go_until_press(0, 6400)
+
+    def stop_stepper(self):
+        """
+        Function that turns off stepper
+        """
+        s0.softStop()
+
+Builder.load_file('stepper1.kv')
+SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
+
+
+if __name__ == "__main__":
+    # send_event("Project Initialized")
+    # Window.fullscreen = 'auto'
+    ProjectNameGUI().run()
+
+
